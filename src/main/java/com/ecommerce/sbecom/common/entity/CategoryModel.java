23251b430e295b81c06d31db818a7af2e3cdf0e2
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2025-01-07 12:51:49 -0500

    Updated pom.xml

--- src/main/java/com/ecommerce/sbecom/common/entity/CategoryModel.java
+++ src/main/java/com/ecommerce/sbecom/common/entity/CategoryModel.java
@@ -1,4 +1,14 @@
 package com.ecommerce.sbecom.common.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class CategoryModel {
 }
