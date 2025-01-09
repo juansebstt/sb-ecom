7cca1e6e9f495072d554fffd34bccf19e0f6613f
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2025-01-09 12:08:06 -0500

    Implemented CategoryRepository

--- src/main/java/com/ecommerce/sbecom/repository/CategoryRepository.java
+++ src/main/java/com/ecommerce/sbecom/repository/CategoryRepository.java
@@ -1,8 +1,9 @@
 package com.ecommerce.sbecom.repository;
 
+import com.ecommerce.sbecom.common.entity.CategoryModel;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
-public interface CategoryRepository extends {
+public interface CategoryRepository extends JpaRepository<CategoryModel, Long> {
 }
