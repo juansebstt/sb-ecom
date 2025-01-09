f4c652a990f051df7be2595fa69f5544e6006a97
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2025-01-09 10:52:22 -0500

    Updated files

--- src/main/java/com/ecommerce/sbecom/common/constant/ApiPathConstant.java
+++ src/main/java/com/ecommerce/sbecom/common/constant/ApiPathConstant.java
@@ -5,7 +5,8 @@ public class ApiPathConstant {
 
     public static final String V1_ROUTE = "/v1"; // Base route for API version 1
     public static final String ADMIN_ROUTE = "/admin"; // Route specific to admin-related endpoints
+    public static final String PUBLIC_ROUTE = "/public"; // Route specific to product-related endpoints
     public static final String CATEGORY_ROUTE = "/category"; // Route specific to category-related endpoints
-    public static final String PUBLIC_ROUTE = "/public"; // Route specific to public endpoints
+    public static final String CATEGORIES_ROUTE = "/categories"; // Route specific to category-related endpoints
 
 }
