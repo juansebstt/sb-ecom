daecc09f5193932902d35539aff287e363abd0c6
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2025-01-09 13:51:20 -0500

    Made some changes

--- src/main/java/com/ecommerce/sbecom/controller/AdminCategoryApi.java
+++ src/main/java/com/ecommerce/sbecom/controller/AdminCategoryApi.java
@@ -1,7 +1,6 @@
 package com.ecommerce.sbecom.controller;
 
 import com.ecommerce.sbecom.common.constant.ApiPathConstant;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping(ApiPathConstant.V1_ROUTE + ApiPathConstant.ADMIN_ROUTE + ApiPathConstant.CATEGORY_ROUTE)
--- src/main/java/com/ecommerce/sbecom/controller/impl/PublicCategoryController.java
+++ src/main/java/com/ecommerce/sbecom/controller/impl/PublicCategoryController.java
@@ -2,15 +2,22 @@ package com.ecommerce.sbecom.controller.impl;
 
 import com.ecommerce.sbecom.common.entity.CategoryModel;
 import com.ecommerce.sbecom.controller.PublicCategoryApi;
+import com.ecommerce.sbecom.service.PublicCategoryService;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 public class PublicCategoryController implements PublicCategoryApi {
 
+    private final PublicCategoryService publicCategoryService;
+
+    public PublicCategoryController(PublicCategoryService publicCategoryService) {
+
+        this.publicCategoryService = publicCategoryService;
+    }
 
     @Override
     public ResponseEntity<List<CategoryModel>> getAllCategories() {
-        return null;
+        return ResponseEntity.ok(publicCategoryService.getAllCategories());
     }
 }
