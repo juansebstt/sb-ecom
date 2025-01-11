3edc482abcb034fef2dd966c2816f14a9a5561a4
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2025-01-11 13:25:10 -0500

    Implementing controllers

--- src/main/java/com/ecommerce/sbecom/controller/AdminCategoryApi.java
+++ src/main/java/com/ecommerce/sbecom/controller/AdminCategoryApi.java
@@ -1,10 +1,14 @@
 package com.ecommerce.sbecom.controller;
 
 import com.ecommerce.sbecom.common.constant.ApiPathConstant;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping(ApiPathConstant.V1_ROUTE + ApiPathConstant.ADMIN_ROUTE + ApiPathConstant.CATEGORY_ROUTE)
 public interface AdminCategoryApi {
 
+    @PostMapping
+    ResponseEntity<?> createCategory();
 
 }
