d268ef22d01a80cd4623f18c9825cfcc1c04b286
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2025-01-15 11:38:56 -0500

    Updated API controllers

--- src/main/java/com/ecommerce/sbecom/controller/impl/AdminCategoryController.java
+++ src/main/java/com/ecommerce/sbecom/controller/impl/AdminCategoryController.java
@@ -1,8 +1,25 @@
 package com.ecommerce.sbecom.controller.impl;
 
+import com.ecommerce.sbecom.common.dto.CategoryDTO;
 import com.ecommerce.sbecom.controller.AdminCategoryApi;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AdminCategoryController implements AdminCategoryApi {
+
+    @Override
+    public ResponseEntity<CategoryDTO> createCategory() {
+        return ResponseEntity.ok();
+    }
+
+    @Override
+    public ResponseEntity<CategoryDTO> updateCategory(Long id, CategoryDTO categoryDTO) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<CategoryDTO> deleteCategory(Long id, CategoryDTO categoryDTO) {
+        return null;
+    }
 }
--- src/main/java/com/ecommerce/sbecom/service/impl/AdminCategoryServiceImpl.java
+++ src/main/java/com/ecommerce/sbecom/service/impl/AdminCategoryServiceImpl.java
@@ -1,8 +1,14 @@
 package com.ecommerce.sbecom.service.impl;
 
+import com.ecommerce.sbecom.common.entity.CategoryModel;
 import com.ecommerce.sbecom.service.AdminCategoryService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AdminCategoryServiceImpl implements AdminCategoryService {
+
+    @Override
+    public CategoryModel createCategory(CategoryModel categoryModel) {
+        return null;
+    }
 }
