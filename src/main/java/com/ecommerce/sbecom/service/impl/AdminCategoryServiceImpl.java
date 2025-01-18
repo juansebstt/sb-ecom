05b8caf8a36ba320a97846f67e0ca9bb4e361dff
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2025-01-18 17:26:31 -0500

    Updated methods signatures

--- src/main/java/com/ecommerce/sbecom/service/impl/AdminCategoryServiceImpl.java
+++ src/main/java/com/ecommerce/sbecom/service/impl/AdminCategoryServiceImpl.java
@@ -37,7 +37,7 @@ public class AdminCategoryServiceImpl implements AdminCategoryService {
     }
 
     @Override
-    public CategoryDTO deleteCategory(CategoryDTO deleteCategory) {
+    public Void deleteCategory(CategoryDTO deleteCategory) {
         return null;
     }
 }
